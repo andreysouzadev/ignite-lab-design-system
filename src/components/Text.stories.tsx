8e8d569293e9293899a0837463af7e16fad55907
Andrey Souza <andreysouzastart@gmail.com>
2022-10-14 14:29:27 -0300

    modified components

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,56 +1,54 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { Text, TextProps } from './text'
+import { Text, TextProps } from './Text'
 
 export default {
-    tittle: 'Components/Text',
-    component: Text,
-    args: {
-      children: 'Lorem ipsum',
-      size: 'md'
-    },
-
-    argTypes: {
-        size: {
-            options: ['sm', 'md', 'lg'],
-            control: {
-                type: 'inline-radio'
-
-            }
-        }
+  title: 'Components/Text',
+  component: Text,
+  args: {
+    children: 'Lorem ipsum.',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'inline-radio'
+      }
     }
-
+  }
 } as Meta<TextProps>
 
 export const Default: StoryObj<TextProps> = {}
 
 export const Small: StoryObj<TextProps> = {
-    args: {
-        size: 'sm'
-    }
+  args: {
+    size: 'sm'
+  }
 }
 
 export const Large: StoryObj<TextProps> = {
-    args: {
-        size: 'lg'
-    }
+  args: {
+    size: 'lg'
+  }
 }
+
 export const CustomComponent: StoryObj<TextProps> = {
-    args: {
-        asChild: true,
-        children: (
-            <p>Text with p tag </p>
-        )
-    },
-    argTypes: {
-        children: {
-            table: {
-                disable: true,
-            }
-        }
+  args: {
+    asChild: true,
+    children: (
+      <p>Text with P tag</p>
+    )
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      }
     },
     asChild: {
-        table: {
-            disable: true, 
-        }
+      table: {
+        disable: true,
+      }
     }
+  }
 }
