a5adcf201fdb6562d7784b3710cb1f2953351702
Andrey Souza <andreysouzastart@gmail.com>
2022-10-14 15:26:11 -0300

    alterating files

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,7 +1,6 @@
 import { themes } from '@storybook/theming'
-import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
 
-import '../src/styles/global.css'
+import '../src/styles/global.css';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -13,5 +12,5 @@ export const parameters = {
   },
   docs: {
     theme: themes.dark,
-  }
+  },
 }
